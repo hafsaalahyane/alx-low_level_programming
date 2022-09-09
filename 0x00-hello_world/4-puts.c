5b18e8302bb45b51d6eba5fd329ab228994094d4
hafsaalahyane <alahyanehafsa@gmail.com>
2022-09-09 11:04:37 -0700

    rask4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
-#!/bin/bash
-int main (void)
+/**
+ * * main - print the string in the put function
+ * *
+ * * Description: using the main function
+ * * this program prints "Programming is like building a multilingual puzzle
+ * * Return: 0
+ * */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	  puts("\"Programming is like building a multilingual puzzle");
+	    return (0);
 }
