e7b0fbe4a1127e17c64993500e35eb20ba152e61
hafsaalahyane <alahyanehafsa@gmail.com>
2022-09-09 11:34:57 -0700

    task 4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,3 @@
-#!/bin/bash
-int main (void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
 #include <stdio.h>
 
 /**
@@ -14,6 +9,6 @@ int main (void)
  * */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
-}}
+	  puts("\"Programming is like building a multilingual puzzle");
+	    return (0);
+}
